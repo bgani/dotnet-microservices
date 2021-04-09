20f76b690d2690649ba6e7cd7108736f8829d26d
bgani <ganibdev@gmail.com>
2021-04-09 18:56:19 +0600

    Clean Architecture -  Ordering.Application layer

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+           
+        }
+    }
+}
