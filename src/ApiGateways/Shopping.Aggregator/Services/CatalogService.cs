b47c073b887c5a82e0093a5ae15f6e09bcaad249
bgani <ganibdev@gmail.com>
2021-05-14 20:02:37 +0600

    Aggregator. Adding service classes to consume internal microservices

---
+++ src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
@@ -0,0 +1,26 @@
+﻿using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        public Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CatalogModel> GetCatalog(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
